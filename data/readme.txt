f5e290ad2cda85f99ccfbe9a96a70d2044f5bc90
Qinghua Zhang <zhangqh24@mails.tsinghua.edu.cn>
2025-09-10 17:11:10 +0800

    Add files via upload

---
+++ data/readme.txt
@@ -0,0 +1,1 @@
+This is the default dataset path read by ITEC. Please place your raw data in this path.
---
+++ examples/demo_data2.m
@@ -0,0 +1,14 @@
+clc;clear;close all;
+
+%% add folders to path
+addpath(genpath('../src/'));
+
+%% set parameters
+csv_path = 'params_data2.csv';
+[paras, paras_instSeg, paras_tracking] = set_paras(csv_path);
+
+%% main function -- Segmentation
+InstanceSegmentation(paras_instSeg);
+
+%% main function -- Tracking
+Tracking(paras_tracking);
